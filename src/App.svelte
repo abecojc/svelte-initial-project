6cad405be36f77a90ff96415803469b9aea64d98
Abeco Jean Claude <73258089+abecojc@users.noreply.github.com>
2021-02-08 13:59:05 -0500

    set up files

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,24 @@
 <script>
-	export let name;
+    import Nested from './Nested.svelte';
+    export let name;
+    let src = "tutorial/image.gif";
+	name = 'Rick Astley';
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+	<h1>Hello {name.toUpperCase()}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+    <img src = { src } alt=" { name } dances.">
+    <p> This is a paragraph.</p>
+    <Nested/>
 </main>
 
 <style>
+    p{
+        color: purple;
+        font-family: 'Comic Sans MS', cursive;
+        font-size: 2em;
+    }
 	main {
 		text-align: center;
 		padding: 1em;
---
+++ src/Nested.svelte
@@ -0,0 +1,11 @@
+<script>
+
+</script>
+
+<main>
+    <p> This is another paragraph.</p>
+</main>
+
+<style>
+
+</style>
